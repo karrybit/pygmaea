c38114242919decce599981ea947c5ff26a3405e
makanai5610 <takumi.k.5610@gmail.com>
2019-08-29 14:03:56 +0900

    add test

--- src/token_type.rs
+++ src/token_type.rs
@@ -48,17 +48,11 @@ impl TokenType {
     }
 
     pub(crate) fn is_int(self) -> bool {
-        match self {
-            TokenType::Int => true,
-            _ => false,
-        }
+        self == TokenType::Int
     }
 
     pub(crate) fn is_eof(self) -> bool {
-        match self {
-            TokenType::EOF => true,
-            _ => false,
-        }
+        self == TokenType::EOF
     }
 }
 
@@ -119,4 +113,64 @@ lazy_static! {
     .iter()
     .cloned()
     .collect();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    fn setup_token_types() -> Vec<TokenType> {
+        use TokenType::*;
+        vec![
+            Plus, Minus, Asterisk, Slash, Assign, Bang, LT, GT, Equal, NotEqual, Comma, Semicolon,
+            LParen, RParen, LBrace, RBrace, True, False, Let, Function, If, Else, Return, Int,
+            Ident, EOF, Illegal,
+        ]
+    }
+
+    fn setup_keyword_token_types() -> Vec<TokenType> {
+        use TokenType::*;
+        vec![True, False, Let, Function, If, Else, Return, Ident]
+    }
+
+    #[test]
+    fn test_is_keyword() {
+        let keyword_token_types = setup_keyword_token_types();
+        setup_token_types().into_iter().for_each(|token_type| {
+            assert_eq!(
+                keyword_token_types.contains(&token_type),
+                token_type.is_keyword()
+            );
+        })
+    }
+
+    #[test]
+    fn test_is_int() {
+        use TokenType::*;
+        setup_token_types().into_iter().for_each(|token_type| {
+            assert_eq!(token_type == Int, token_type.is_int());
+        })
+    }
+
+    #[test]
+    fn test_is_eof() {
+        use TokenType::*;
+        setup_token_types().into_iter().for_each(|token_type| {
+            assert_eq!(token_type == EOF, token_type.is_eof());
+        })
+    }
+
+    #[test]
+    fn test_default() {
+        use TokenType::*;
+        let token_type: TokenType = Default::default();
+        assert_eq!(token_type, Illegal);
+    }
+
+    #[test]
+    fn test_keyword() {
+        let keyword_token_types = setup_keyword_token_types();
+        KEYWORDS.values().for_each(|keyword| {
+            assert!(keyword_token_types.contains(keyword));
+        })
+    }
 }
